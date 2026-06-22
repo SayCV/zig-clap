9875bcac17fd32d30b38b1ce35122f136b4fa438
SayCV <sssxyc@163.com>
2026-06-22 13:53:30 +0800

    bump to zig 0.17.0

--- clap.zig
+++ clap.zig
@@ -1232,7 +1232,7 @@ test "overflow-safe" {
     );
 
     var iter = args.SliceIterator{
-        .args = &(.{"-" ++ ("a" ** 300)}),
+        .args = &(.{"-" ++ ("a" * *300)}),
     };
 
     // This just needs to not crash
